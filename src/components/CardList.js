7111dd8a2efd46d9f26cbd32a13216bc654efa4e
mariobermudezjr <mbermudez91@gmail.com>
2018-08-24 13:14:50 -0700

    chore: Add a catch for empty list and force an error throw within Card List. Note that in development mode the page will not show the actually page but the detailed error code. In production build it will show the fancy error message.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error('Noooo!');
+  }
   const cardsArray = robots.map((user, i) => {
     return <Card key={user.id} id={user.id} name={user.name} email={user.email} />;
   });
