ce3ccf5c5adf2d41848fdb33d204ed7f55ee47ab
mariobermudezjr <mbermudez91@gmail.com>
2018-08-27 15:42:06 -0700

    refactor: Add the vanilla fetch request to a redux action.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 import './index.css';
 import App from './containers/App';
-
 import registerServiceWorker from './registerServiceWorker';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
