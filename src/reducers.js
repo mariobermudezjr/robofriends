f050f9a2fb0c867f3ac066b4bb15d26333b5b0ad
mariobermudezjr <mbermudez91@gmail.com>
2018-08-27 14:50:11 -0700

    refactor: Add redux and react-redux to our application replacing local this.state for our searchfield data

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+import { CHANGE_SEARCH_FIELD } from './constants';
+
+const INITIAL_STATE = {
+  searchField: ''
+};
+
+export const searchRobots = (state = INITIAL_STATE, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+};
